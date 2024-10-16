1dd3aba69314922a5a768c5131c6fb233263e29d
Preston <prestonosoro56@gmail.com>
2024-10-16 23:02:35 +0300

    feat: Implement Error Boundary Component
    - Created an ErrorBoundary component to handle errors in child components
    - Added fallback UI to enhance user experience during errors
    - Integrated the ErrorBoundary component in the main application

---
+++ frontend/src/components/Errorboundary/Errorboundary.tsx
@@ -0,0 +1,34 @@
+import React, { ErrorInfo } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(_error: Error, errorInfo: ErrorInfo) {
+    console.error("Error Boundary caught an error:", _error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
