d66ac51ad1d1a7754b29e6a6ae66e54194b4fac0
Preston <prestonosoro56@gmail.com>
2024-10-17 05:45:37 +0300

    feat(Routing): add protected route for users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./components/Home/Home";
 import Users from "./components/features/users/Users";
 import Album from "./components/features/album/Album";
@@ -7,6 +12,8 @@ import Photo from "./components/features/photos/Photo";
 import Login from "./components/features/login/Login";
 import Navbar from "./components/Navbar/Navbar";
 import ErrorBoundary from "./components/Errorboundary/Errorboundary";
+import ProtectedRoute from "./components/features/login/ProtectedRoute/ProtectedRoute";
+import SignUp from "./components/features/signup/SignUp";
 import "./App.css";
 
 const App: React.FC = () => {
@@ -15,11 +22,15 @@ const App: React.FC = () => {
       <Navbar />
       <ErrorBoundary>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Navigate to="/login" />} />{" "}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<ProtectedRoute component={Home} />} />
+          <Route
+            path="/users"
+            element={<ProtectedRoute component={Users} />}/>
           <Route path="/albums/:albumId" element={<Album />} />
           <Route path="/photos/:photoId" element={<Photo />} />
-          <Route path="/login" element={<Login />} />
         </Routes>
       </ErrorBoundary>
     </Router>
