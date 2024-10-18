e31f1a75868304e440ea9b6f5c7acc337d560c78
Preston <prestonosoro56@gmail.com>
2024-10-18 13:34:06 +0300

    fix: fix types for user

---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,19 @@
+import { Request } from 'express';
+
+export interface CustomBody {
+  name?: string;
+  username?: string;
+  email?: string;
+  userId?: string;
+  title?: string;
+  album?: string;
+  url?: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      body: CustomBody; 
+    }
+  }
+}
