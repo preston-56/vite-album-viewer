41395b6764b935e0a862d98a0ac0bb3be036f47c
Preston <prestonosoro56@gmail.com>
2024-10-18 04:07:48 +0300

    feat:update import

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import {sequelize} from "./config/config";
-import { initModels } from './models/index.js';
+import { initModels } from './models/index';
 
 import User from './models/User';
 import Album from './models/Album';
