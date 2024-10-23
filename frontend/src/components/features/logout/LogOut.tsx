c1a697594d39a905c6e086ec6e317996b5834bbf
Preston <prestonosoro56@gmail.com>
2024-10-24 00:38:50 +0300

    refactor(LogOut): Remove color scheme from Logout button
    - Eliminated the color scheme definition from the Logout button as it is now consistently styled in the Navbar.
    - Ensured that the button retains its solid variant and large size for uniformity with other buttons in the application.

--- frontend/src/components/features/logout/LogOut.tsx
+++ frontend/src/components/features/logout/LogOut.tsx
@@ -35,7 +35,7 @@ const LogOut: React.FC = () => {
   };
 
   return (
-    <Button colorScheme="blue" variant="solid" onClick={handleLogout} size="lg">
+    <Button variant="solid" onClick={handleLogout} size="lg">
       Logout
     </Button>
   );
