b837fcb11e5927cdc9693cc1da85b7fc1710aa3a
Preston <prestonosoro56@gmail.com>
2024-10-24 21:23:43 +0300

    fix: import app components from module
    - Imported `create_app` and `db` from `.app`.
    - Imported `Config` from `.config`.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,35 +1,3 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from dotenv import load_dotenv
-
-load_dotenv()
-
-db = SQLAlchemy()
-migrate = Migrate()
-
-def create_app():    
-    app = Flask(__name__)        
-    
-    app.config.from_mapping(
-        SECRET_KEY=os.getenv('SECRET_KEY', 'My_Secret_Key'),
-        SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL'),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
-    )     
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    from app.routes import user_routes, album_routes, photo_routes
-
-    app.register_blueprint(user_routes)
-    app.register_blueprint(album_routes)
-    app.register_blueprint(photo_routes)
-
-    return app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
-
+from .app import create_app
+from .app import db
+from .config import Config
