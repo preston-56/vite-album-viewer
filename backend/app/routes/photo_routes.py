4e39be5823e511f8cc430b8ec89c2f664c8ea900
Preston <prestonosoro56@gmail.com>
2024-10-24 21:26:32 +0300

    feat: implement photo routes for CRUD operations
    - Added route to retrieve all photos associated with a specific album.
    - Implemented route to fetch a single photo by ID.
    - Created route to upload a new photo to an album, validating required fields.
    - Implemented route to update an existing photo.
    - Added route to delete a photo by ID, handling non-existent photos.

--- backend/app/routes/photo_routes.py
+++ backend/app/routes/photo_routes.py
@@ -1,14 +1,10 @@
 from flask import Blueprint, request, jsonify, abort
 from app.models.photo import Photo
+from app.models.album import Album
 from app import db
 
 photo_routes = Blueprint('photo_routes', __name__)
 
-
-@photo_routes.route('/api/photos', methods=['GET'])
-def get_photos():
-    photos = Photo.query.all()
-    return jsonify([{ 'id': photo.id, 'album_id': photo.album_id, 'title': photo.title, 'url': photo.url } for photo in photos])
 
 @photo_routes.route('/api/photos/<int:id>', methods=['GET'])
 def get_photo_by_id(id):
@@ -25,14 +21,12 @@ def get_photo_by_id(id):
 @photo_routes.route('/api/photos', methods=['GET'])
 def get_photos_by_album_id():
     album_id = request.args.get('album_id')  # Get album_id from query parameters
-    
     if album_id:
         photos = Photo.query.filter_by(album_id=album_id).all()
         if not photos:
             return jsonify({'error': 'No photos found for this album'}), 404
     else:
-        return jsonify({'error': 'album_id parameter is required'}), 400
-    
+        photos = Photo.query.all()
     return jsonify([{ 
         'id': photo.id, 
         'album_id': photo.album_id, 
@@ -52,12 +46,49 @@ def update_photo(id):
     data = request.get_json()
     photo = db.session.get(Photo, id) 
     if photo is None:
-        return jsonify({'error': 'Photo not found'}), 404  
-    photo.album_id = data['album_id']
+        return jsonify({'error': 'Photo not found'}), 404 
+
+    # Update the photo title
     photo.title = data['title']
-    photo.url = data['url']
     db.session.commit()
-    return jsonify({ 'id': photo.id, 'album_id': photo.album_id, 'title': photo.title, 'url': photo.url })
+
+    # Get the album to retrieve user_id
+    album = Album.query.get(photo.album_id)
+    user_id = album.user_id if album else None
+
+    # Return the updated photo along with the user_id
+    return jsonify({
+        'id': photo.id,
+        'title': photo.title,
+        'album_id': photo.album_id,
+        'user_id': user_id  # Include user_id in the response
+    })
+
+
+@photo_routes.route('/api/photos/<int:id>', methods=['PATCH'])
+def patch_photo(id):
+    data = request.get_json()
+    photo = db.session.get(Photo, id)
+    if photo is None:
+        return jsonify({'error': 'Photo not found'}), 404
+    
+    if 'title' in data:
+        photo.title = data['title']
+    
+    if 'album_id' in data:
+        photo.album_id = data['album_id']
+    
+    if 'url' in data:
+        photo.url = data['url']
+    
+    db.session.commit()
+    return jsonify({
+        'id': photo.id,
+        'album_id': photo.album_id,
+        'title': photo.title,
+        'url': photo.url
+    })
+
 
 @photo_routes.route('/api/photos/<int:id>', methods=['DELETE'])
 def delete_photo(id):
