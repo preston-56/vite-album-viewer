e0a2a715b561ef76a3c3f9ede424a808cef1f670
Preston <prestonosoro56@gmail.com>
2024-10-27 00:20:07 +0300

    refactor: Organize App Structure and Routing
    - Implemented React Router for seamless navigation across the application.
    - Established a centralized authentication context using `AuthProvider` for managing user sessions.
    - Integrated `ErrorBoundary` for enhanced error handling and user experience.
    - Set up protected routes for Home, Users, User Albums, and Edit Photo components to restrict access based on authentication.
    - Simplified route definitions for clarity and maintainability.
    - Added a default redirect from the root path to the login page.
    - Improved overall code organization by separating components for better readability and structure.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,6 @@ import {
 } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Users from "./components/features/users/Users";
-import Album from "./components/features/album/Album";
-import Photo from "./components/features/photos/Photo";
 import Login from "./components/features/login/Login";
 import Navbar from "./components/Navbar/Navbar";
 import ErrorBoundary from "./components/Errorboundary/Errorboundary";
@@ -16,7 +14,7 @@ import ProtectedRoute from "./components/features/login/ProtectedRoute/Protected
 import SignUp from "./components/features/signup/SignUp";
 import EditPhoto from "./components/features/editphoto/EditPhoto";
 import NotFound from "./components/features/404/NotFound";
-import UserAlbums from "./components/features/users/UserAlbums";
+import UserAlbums from "./components/albums/UserAlbums";
 import { AuthProvider } from "./components/AuthContext/AuthContext";
 import "./App.css";
 
@@ -28,27 +26,13 @@ const App: React.FC = () => {
       <Navbar/>
       <ErrorBoundary>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />{" "}
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} /> 
           <Route path="/signup" element={<SignUp />} />
           <Route path="/home" element={<ProtectedRoute component={Home} />} />
           <Route path="/users" element={<ProtectedRoute component={Users} />} />
-          <Route
-            path="/albums/:albumId"
-            element={<ProtectedRoute component={Album} />}
-          />
-          <Route
-            path="/users/:userId/albums/"
-            element={<ProtectedRoute component={UserAlbums} />}
-          />
-          <Route
-            path="/photos/:photoId"
-            element={<ProtectedRoute component={Photo} />}
-          />
-          <Route
-            path="/edit-photo/:photoId"
-            element={<ProtectedRoute component={EditPhoto} />}
-          />
+          <Route path="/users/:user_id/albums/" element={<ProtectedRoute component={UserAlbums} />} />
+          <Route path="/edit-photo/:photo_id/" element={<ProtectedRoute component={EditPhoto} />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </ErrorBoundary>
