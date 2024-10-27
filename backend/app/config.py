449d54be5b68b35cac6baf20cbb775d014381395
Preston <prestonosoro56@gmail.com>
2024-10-27 21:34:17 +0300

    feat: add fallbacl for testing

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     """Base configuration."""
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///:memory:')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_ALLOWED_ORIGINS = [
         "*"  
